bebe58a58b9ee86dc3e2843798c6cadf89c765b0
Frank Hoedemakers <fhoedemakers@hotmail.com>
2023-08-14 17:37:59 +0200

    multiple changes

--- httpclient.c
+++ httpclient.c
@@ -5,7 +5,7 @@
 char myBuff[4096];
 
 void result(void *arg, httpc_result_t httpc_result,
-        u32_t rx_content_len, u32_t srv_res, err_t err)
+            u32_t rx_content_len, u32_t srv_res, err_t err)
 
 {
     printf("\ntransfer complete\n");
@@ -13,8 +13,8 @@ void result(void *arg, httpc_result_t httpc_result,
     printf("http result=%d\n", srv_res);
 }
 
-err_t headers(httpc_state_t *connection, void *arg, 
-    struct pbuf *hdr, u16_t hdr_len, u32_t content_len)
+err_t headers(httpc_state_t *connection, void *arg,
+              struct pbuf *hdr, u16_t hdr_len, u32_t content_len)
 {
     printf("\nheaders recieved\n");
     printf("content length=%d\n", content_len);
@@ -25,8 +25,8 @@ err_t headers(httpc_state_t *connection, void *arg,
     return ERR_OK;
 }
 
-err_t body(void *arg, struct altcp_pcb *conn, 
-                            struct pbuf *p, err_t err)
+err_t body(void *arg, struct altcp_pcb *conn,
+           struct pbuf *p, err_t err)
 {
     printf("body\n");
     pbuf_copy_partial(p, myBuff, p->tot_len, 0);
@@ -36,10 +36,10 @@ err_t body(void *arg, struct altcp_pcb *conn,
     return ERR_OK;
 }
 
-int setup(uint32_t country, const char *ssid, 
-   const char *pass, uint32_t auth, 
-     const char *hostname, ip_addr_t *ip,
-                   ip_addr_t *mask, ip_addr_t *gw)
+int setup(uint32_t country, const char *ssid,
+          const char *pass, uint32_t auth,
+          const char *hostname, ip_addr_t *ip,
+          ip_addr_t *mask, ip_addr_t *gw)
 {
     if (cyw43_arch_init_with_country(country))
     {
@@ -52,7 +52,8 @@ int setup(uint32_t country, const char *ssid,
         netif_set_hostname(netif_default, hostname);
     }
 
-    if (cyw43_arch_wifi_connect_async(ssid, pass, auth))
+    // if (cyw43_arch_wifi_connect_async(ssid, pass, auth))
+    if (cyw43_arch_wifi_connect_timeout_ms(ssid, pass, auth, 10000))
     {
         return 2;
     }
@@ -63,19 +64,18 @@ int setup(uint32_t country, const char *ssid,
     {
         printf("Retrying connection\n");
         int new_status = cyw43_tcpip_link_status(
-             &cyw43_state,CYW43_ITF_STA);
+            &cyw43_state, CYW43_ITF_STA);
         if (new_status != status)
         {
             status = new_status;
             flashrate = flashrate / (status + 1);
-            printf("connect status: %d %d\n", 
-                                    status, flashrate);
+            printf("connect status: %d %d\n",
+                   status, flashrate);
         }
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         sleep_ms(flashrate);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
         sleep_ms(flashrate);
-        
     }
     if (status < 0)
     {
@@ -97,54 +97,76 @@ int setup(uint32_t country, const char *ssid,
             netif_set_gw(netif_default, gw);
         }
         printf("IP: %s\n",
-        ip4addr_ntoa(netif_ip_addr4(netif_default)));
-        printf("Mask: %s\n", 
-        ip4addr_ntoa(netif_ip_netmask4(netif_default)));
+               ip4addr_ntoa(netif_ip_addr4(netif_default)));
+        printf("Mask: %s\n",
+               ip4addr_ntoa(netif_ip_netmask4(netif_default)));
         printf("Gateway: %s\n",
-        ip4addr_ntoa(netif_ip_gw4(netif_default)));
+               ip4addr_ntoa(netif_ip_gw4(netif_default)));
         printf("Host Name: %s\n",
-        netif_get_hostname(netif_default));
+               netif_get_hostname(netif_default));
     }
     return status;
 }
 #ifndef WIFI_SSID
 #define WIFI_SSID "WIFI_SSID"
 #endif
-#ifndef WIFI_PASS
-#define WIFI_PASS "WIFI_PASS"
+#ifndef WIFI_PASSWORD
+#define WIFI_PASSWORD "WIFI_PASS"
 #endif
 char ssid[] = WIFI_SSID;
-char pass[] = WIFI_PASS;
+char pass[] = WIFI_PASSWORD;
 uint32_t country = CYW43_COUNTRY_NETHERLANDS;
 uint32_t auth = CYW43_AUTH_WPA2_MIXED_PSK;
 int main()
 {
     stdio_init_all();
-      for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 3; i++)
     {
         printf("Wake-up USB\n");
         sleep_ms(1000);
     }
-    setup(country, ssid, pass, auth, 
-               "MyPicoW", NULL, NULL, NULL);
-    uint16_t port = 80;
-    httpc_connection_t settings;
-    
-    settings.result_fn = result;
-    settings.headers_done_fn = headers;
+    printf("SSID: %s\n", ssid);
+    printf("PASS: %s\n", pass);
 
-    err_t err = httpc_get_file_dns(
-            "example.com",
+    printf("Country: %s\n", WIFI_COUNTRY);
+    printf("Remote domain: %s\n", REMOTE_DOMAIN);
+    printf("Relative url: %s\n", RELATIVE_URL);
+    if (strlen(WIFI_COUNTRY) == 2)
+    {
+        country = CYW43_COUNTRY(WIFI_COUNTRY[0],
+                                WIFI_COUNTRY[1], 0);
+    }
+    else
+    {
+        printf("Invalid country code, halting\n");
+        while (1)
+        {
+            tight_loop_contents();
+        }
+    }
+
+    if (setup(country, ssid, pass, auth,
+              "MyPicoW", NULL, NULL, NULL) == CYW43_LINK_UP)
+    {
+        uint16_t port = 80;
+        httpc_connection_t settings;
+
+        settings.result_fn = result;
+        settings.headers_done_fn = headers;
+
+        err_t err = httpc_get_file_dns(
+            REMOTE_DOMAIN,
             80,
-            "/index.html",
+            RELATIVE_URL,
             &settings,
             body,
             NULL,
-            NULL
-        ); 
+            NULL);
 
-    printf("status %d \n", err);
-    while (true){
+        printf("status %d \n", err);
+    }
+    while (true)
+    {
         sleep_ms(500);
     }
 }
---
+++ wifiparams.cmake
@@ -0,0 +1,8 @@
+# Put this file in your home folder
+set(WIFI_SSID "your ssid")
+set(WIFI_PASSWORD "your password")
+# change according to your country code
+set(WIFI_COUNTRY "NL")
+# url to retrieve ( http://REMOTE_DOMAIN/RELATIVE_URL )
+set(REMOTE_DOMAIN "example.com")
+set(RELATIVE_URL  "/index.html")
